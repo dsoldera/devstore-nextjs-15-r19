c543430208434922c3e6603289c67f195f695727
dsoldera <daniellesoldera@gmail.com>
2025-06-20 13:58:08 -0300

    fix nextjs issue

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,19 +1,23 @@
 'use client'
+
+import { Suspense } from 'react'
+
 //useSearchParams only be used on client side
+//import { useSearchParams } from 'next/navigation'
 import { Skeleton } from '@/components/skeleton'
-import { useSearchParams } from 'next/navigation'
+import { CurrentSearch } from './current-search'
 
 export default function SearchLoading() {
   //useSearchParams is a Client Component
-  const searchParams = useSearchParams()
+  //const searchParams = useSearchParams()
 
-  const query = searchParams.get('q')
+  //const query = searchParams.get('q')
 
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">{query}</span>
-      </p>
+      <Suspense fallback={null}>Add commentMore actions
+        <CurrentSearch />
+      </Suspense>
 
       <div className="grid grid-cols-3 gap-6">
         <Skeleton className="h-[400px]" />
